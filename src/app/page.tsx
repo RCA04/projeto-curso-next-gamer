c098858f7a134d30be7bf2f7cce1af1b021bc122
RCA04 <ryan04.dev@gmail.com>
2025-10-01 14:59:19 -0300

    develop: fetch da variável de ambiente, mas com error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+import Container from "@/components/container";
 
-export default function Home() {
+async function getDailyGames() {
+  try{
+    const res = await fetch(``);
+
+    return res.json
+
+  }catch(error){
+    throw new Error('failed to fetch data')
+  }
+}
+
+
+
+export default async function Home() {
+  const dailyGames = await getDailyGames();
+  console.log(dailyGames);
   return (
-    <div className="">
-    <h1>test</h1>
-    </div>
+    <main className="flex">
+      <Container>
+         <h1 className="text-center font-bold text-xl mt-10 mb-7">Separamos jogos exclusivos para você</h1>
+      </Container>
+      </main>
   );
 }
