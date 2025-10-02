0c4a8306584fa4dad2a3fad75bcf927666d2a132
RCA04 <ryan04.dev@gmail.com>
2025-10-02 11:15:34 -0300

    develop: adição das configurações do componente de images next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images:{
+    qualities: [75, 85, 95, 100],
+    domains: ['sujeitoprogramador.com'],
+  }
 };
 
 export default nextConfig;
