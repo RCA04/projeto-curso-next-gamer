fa1cb6f18bc5bec813d5c4c6f32c07a3c1739718
RCA04 <ryan04.dev@gmail.com>
2025-10-02 11:17:11 -0300

    fix: correção do fetch e alteração do nome da função

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,28 @@
 import Container from "@/components/container";
+import { GameProps } from "@/utils/types/games";
+import Link from "next/link";
+import Image from "next/image";
 
-async function getDailyGames() {
-  try{
-    const res = await fetch(``);
-
-    return res.json
-
-  }catch(error){
-    throw new Error('failed to fetch data')
+async function getDalyGames() {
+ 
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/next-api/?api=game_day`, {cache: "no-store"});
+  if(!res.ok){
+    throw new Error("Failed to fetch data");
   }
+  
+  return res.json();
 }
 
 
-
 export default async function Home() {
-  const dailyGames = await getDailyGames();
+  const dailyGames: GameProps = await getDalyGames();
   console.log(dailyGames);
   return (
     <main className="flex">
       <Container>
+         
          <h1 className="text-center font-bold text-xl mt-10 mb-7">Separamos jogos exclusivos para você</h1>
+
       </Container>
       </main>
   );
