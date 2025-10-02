4ea3e30153358ffb9b3138d79e090a12511b67cc
RCA04 <ryan04.dev@gmail.com>
2025-10-02 16:33:14 -0300

    fix: correção do nome do dado que é recebido

--- src/utils/types/games.ts
+++ src/utils/types/games.ts
@@ -4,7 +4,7 @@ export interface GameProps{
     title:string;
     description:string;
     image_url:string;
-    platform: string[],
+    platforms: string[],
     categories:string[],
     release:string;
 }
