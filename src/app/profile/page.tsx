38a051042bcf5dd4f4783af1962d8231511b7495
RCA04 <ryan04.dev@gmail.com>
2025-10-02 17:29:44 -0300

    develop: início da página de perfil

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/container";
+import Image from "next/image";
+
+export default function Profile(){
+    return(
+        <main className="w-full  text-black">
+        <Container>
+             
+
+        </Container>
+        </main>
+    )
+}
