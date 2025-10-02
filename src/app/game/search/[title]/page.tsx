9631c190432ef7b15d4b1a3997a21d58a7ddf8d4
RCA04 <ryan04.dev@gmail.com>
2025-10-02 14:44:56 -0300

    develop: adição da página de pesquisa de jogos

---
+++ src/app/game/search/[title]/page.tsx
@@ -0,0 +1,46 @@
+import { GameProps } from "@/utils/types/games";
+import  Container  from "@/components/container";
+import { Input } from "@/components/input";
+import { GameCard } from "@/components/gameCard";
+
+async function getData(title: string){
+try{
+ const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/next-api/?api=game&title=${title}`)
+    return res.json();
+}catch(error){
+    return null;
+}
+
+
+}
+
+export default async function Search({
+    params:{title}
+}:{
+    params: {title:string}
+}){
+
+    const games: GameProps[] = await getData(title);
+
+    return(
+    <main>
+    <Container>
+
+    <Input/>
+    
+    <h1 className="font-bold text-xl mt-8 mb-5">Veja o que encontramos na nossa base</h1>
+    
+        {!games &&(
+            <p>esse jogo não foi encontrado</p>
+        )}
+
+         <section className="grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+           {games && games.map((i)=>(
+             <GameCard key={i.id} data={i}/>
+           ))}
+         </section>
+    </Container>
+    </main>
+)
+
+}
