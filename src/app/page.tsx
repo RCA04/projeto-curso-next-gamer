a66143a0be2be5dde8565925c8b320a0eb4544a2
RCA04 <ryan04.dev@gmail.com>
2025-10-02 13:33:31 -0300

    develop: adição do input de pesquisa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { GameProps } from "@/utils/types/games";
 import Link from "next/link";
 import Image from "next/image";
 import { BsArrowRightSquare} from 'react-icons/bs'
+import { Input } from "@/components/input";
 
 async function getDalyGames() {
  
@@ -41,7 +42,9 @@ export default async function Home() {
            </div>
           </section>
          </Link>
-      
+
+        <Input />
+
       </Container>
       </main>
   );
