412f64a9a5a296812f349204a9dbd47f6d4f98a8
RCA04 <ryan04.dev@gmail.com>
2025-10-02 14:59:04 -0300

    develop: decode da url e organização do código

--- src/app/game/search/[title]/page.tsx
+++ src/app/game/search/[title]/page.tsx
@@ -5,13 +5,12 @@ import { GameCard } from "@/components/gameCard";
 
 async function getData(title: string){
 try{
- const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/next-api/?api=game&title=${title}`)
+ const decodeTitle = decodeURI(title)
+ const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/next-api/?api=game&title=${decodeTitle}`)
     return res.json();
 }catch(error){
     return null;
-}
-
-
+ }
 }
 
 export default async function Search({
@@ -21,24 +20,19 @@ export default async function Search({
 }){
 
     const games: GameProps[] = await getData(title);
-
     return(
     <main>
     <Container>
-
-    <Input/>
-    
-    <h1 className="font-bold text-xl mt-8 mb-5">Veja o que encontramos na nossa base</h1>
-    
-        {!games &&(
-            <p>esse jogo não foi encontrado</p>
-        )}
-
-         <section className="grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-           {games && games.map((i)=>(
-             <GameCard key={i.id} data={i}/>
-           ))}
-         </section>
+        <Input/>
+        <h1 className="font-bold text-xl mt-8 mb-5">Veja o que encontramos na nossa base</h1>
+            {!games &&(
+                <p>esse jogo não foi encontrado</p>
+            )}
+        <section className="grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {games && games.map((i)=>(
+                <GameCard key={i.id} data={i}/>
+            ))}
+        </section>
     </Container>
     </main>
 )
