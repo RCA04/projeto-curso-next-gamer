2b0f25bba5fee50675151bd168f6d1495a266f44
RCA04 <ryan04.dev@gmail.com>
2025-10-02 15:42:00 -0300

    develop: criação e inicio da página de detalhes do jogos

---
+++ src/app/game/[id]/page.tsx
@@ -0,0 +1,50 @@
+import { GameProps } from "@/utils/types/games";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import Container from "@/components/container";
+
+async function getData(id:string){
+    
+    try{
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/next-api/?api=game&id=${id}`)
+        return res.json();
+    }catch(error){
+        throw new Error('Failed to fetch data')
+    }
+}
+
+export default async function game({
+    params:{id}
+
+}: {
+    params: {id:string}
+}
+){
+    const data:GameProps = await getData(id);
+
+    if(!data){
+        redirect('/')
+    }
+
+    return(
+        <main className="w-full text-black">
+            <div className="bg-black w-full h-80 sm:h-96 relative">
+            <Image
+            className="object-cover w-full h-80 sm:h-96 opacity-80"
+            src={data.image_url}
+            alt={data.title}
+            priority={true}
+            fill={true}
+            quality={100}
+            sizes="(max-width: 768px) 100vw, (min-width: 1200px) 33vw"
+            />
+            </div>
+        <Container>
+            <h1 className="font-bold text-xl my-4">{data.title}</h1>
+            <p>{data.description}</p>
+        </Container>
+        
+            
+        </main>
+    )
+}
